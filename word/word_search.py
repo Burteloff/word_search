1dc16362a3844061d0647bcef117529e78e8dd9d
Burtelofff <38983111+Kwlski@users.noreply.github.com>
2022-01-17 02:42:28 +0300

    add ->one_letter - словарь одной буквы и следующей за ней, с количеством раз, при которых следующая буква встречается вместе с моей one_letter[prev_char]=count -> prev_char первый символ

--- word/word_search.py
+++ word/word_search.py
@@ -1,1 +1,15 @@
 file_name=input("Введите название файла Пример: text.txt\n") #название файла
+one_letter={} #словарь из, которого вытягиваем одну букву и последующую за ним
+prev_char='' #первый символ для поиска в словаре
+with open(file_name,'r',encoding='utf8') as file: #открываем файлик и добавляем символ + символ последующий за ним в словарь stat
+    for line in file:
+        for char in line:
+            if prev_char in one_letter:
+                if char in one_letter[prev_char]:
+                    one_letter[prev_char][char]+=1
+                else:
+                    one_letter[prev_char][char]=1
+            else:
+                one_letter[prev_char]={char:1}
+            prev_char=char
+stats2={}
