4b1633f5b3b12ca0235836d95a0b2e2451dda765
Burtelofff <38983111+Kwlski@users.noreply.github.com>
2022-01-17 03:02:48 +0300

    добавил функцию поиск слова (недоделал)

--- word/word_search.py
+++ word/word_search.py
@@ -12,17 +12,37 @@ with open(file_name,'r',encoding='utf8') as file: #открываем файли
             else:
                 one_letter[prev_char]={char:1}
             prev_char=char
-stats2={}
-def max_char(keys): #Ищет максимально встречаемый символ
-    max_count=0 #максимальный в данный момент
-    max_char= ''
-    for key in keys:
-        if((keys[key])>max_count): #поиск максимального
-            max_count=keys[key]
-            max_char=key
-    return max_char
+
+
+def word_search(current_word, count_sym): #поиск слова
+    with open(file_name, 'r', encoding='utf8') as file:
+        for line in file:
+            count_char_line = 0
+            for char in line:
+                if(current_word[0]==char):
+                    while n < count_sym:
+                        if (count_char_line != len(line) - (count_sym - 1)):
+                            if(check_partline(current_word, line, count_sym, count_char_line)==True):
+                                line1_cur=line[count_char_line + count_sym]
+                                word= str(current_word) + line1_cur
+                                if (current_word not in more_letter):
+                                    more_letter[current_word] ={word:1}
+                                else:
+                                    if(word in more_letter[current_word]):
+                                        more_letter[current_word][word]+=1
+                                    else:
+                                        more_letter[current_word][word]=1
+
+                count_char_line+=1
+
+    new_word=max_char(more_letter[current_word])
+
+    more_letter.clear()
+    return new_word
+
+more_letter={}
 word_zero=input("Введите букву: ") #Вводим первую букву из, которого составится слово
 word_second = word_zero+max_char(one_letter[word_zero]) #слово с двумя буквами, вторая наиболее часто встречаемая
 while True:
-
+    word_more = word_search(word2, ne)
 #word_second=
